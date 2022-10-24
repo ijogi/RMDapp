b4ce2a27850a9c292c21dfc16e290156af13163a
Indrek Jõgi <indrek.jogi@single.earth>
2022-10-24 19:36:15 +0200

    Added Marketplace contract for listing and selling NFTs

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+error NotTokenOwner(address nftAddress, uint256 tokenId);
+error ItemNotListed(address nftAddress, uint256 tokenId);
+error ItemAlreadyListed(address nftAddress, uint256 tokenId);
+error PriceMustBeAboveZero(uint256 price);
+error ValueDoesNotMatchPrice(address nftAddress, uint256 tokenId, uint256 price);
+error NftNotApprovedForMarketplace(address nftAddress, uint256 tokenId);
+
+contract MarketPlace is ReentrancyGuard {
+    struct ListedItem {
+        uint256 price;
+        address seller;
+    }
+
+    mapping(address => mapping(uint256 => ListedItem)) private _listedItems;
+
+    modifier isTokenOwner(address nftAddress, uint256 tokenId, address initiator) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (initiator != owner) {
+            revert NotTokenOwner(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier notListed(address nftAddress, uint256 tokenId) {
+        ListedItem memory item = _listedItems[nftAddress][tokenId];
+        if (item.price > 0) {
+            revert ItemNotListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        ListedItem memory item = _listedItems[nftAddress][tokenId];
+        if (item.price <= 0) {
+            revert ItemAlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    function listItem(address nftAddress, uint256 tokenId, uint256 price) 
+        external 
+        notListed(nftAddress, tokenId) 
+        isTokenOwner(nftAddress, tokenId, msg.sender) 
+    {
+        if (price <= 0) {
+            revert PriceMustBeAboveZero(price);
+        }
+        IERC721 nft = IERC721(nftAddress);
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert PriceMustBeAboveZero(price);
+        }
+        _listedItems[nftAddress][tokenId] = ListedItem(price, msg.sender);
+    }
+
+    function buyItem(address nftAddress, uint256 tokenId)
+        external 
+        payable 
+        isListed(nftAddress, tokenId)
+        nonReentrant
+    {
+        ListedItem memory item = _listedItems[nftAddress][tokenId];
+        if (msg.value < item.price) {
+            revert ValueDoesNotMatchPrice(nftAddress, tokenId, item.price);
+        }
+        (bool success, ) = payable(msg.sender).call{value: msg.value}("");
+        require(success, "Transfer failed");
+        delete _listedItems[nftAddress][tokenId];
+        IERC721(nftAddress).safeTransferFrom(item.seller, msg.sender, tokenId);
+    }
+
+    function cancelListing(address nftAddress, uint256 tokenId) 
+        external 
+        isTokenOwner(nftAddress, tokenId, msg.sender)
+        isListed(nftAddress, tokenId)
+    {
+        delete _listedItems[nftAddress][tokenId];
+    }
+
+    function updateItemPrice(address nftAddress, uint256 tokenId, uint256 newPrice) 
+        external 
+        isTokenOwner(nftAddress, tokenId, msg.sender) 
+        isListed(nftAddress, tokenId)
+    {
+        if (newPrice <= 0) {
+            revert PriceMustBeAboveZero(newPrice);
+        }
+        _listedItems[nftAddress][tokenId].price = newPrice;
+    }
+}
--- rm-dapp/src/App.tsx
+++ rm-dapp/src/App.tsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
-import './App.css'
-import RobotMuralist from './artifacts/contracts/RobotMuralist.sol/RobotMuralist.json'
+import React, { useEffect, useState } from "react"
+import { ethers } from "ethers"
+import "./App.css"
+import RobotMuralist from "./artifacts/contracts/RobotMuralist.sol/RobotMuralist.json"
 
 const ROBOTMURALIST_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 function App() {
   type extendedWindow = Window & typeof globalThis & { ethereum: any }
-  type ethAddress = `0x${string}`
+
   const [accounts, setAccounts] = useState([])
   const [provider] = useState(new ethers.providers.Web3Provider((window as extendedWindow).ethereum))
+  const [address, setAddress] = useState("")
+  const [tokenUri, setTokenUri] = useState("")
 
   console.log(RobotMuralist)
 
@@ -21,10 +23,10 @@ function App() {
     connectToMetamask()
   }, [provider])
 
-  async function mintNft(addressTo: ethAddress, tokenUri: string) {
+  async function mintNft() {
     const signer = provider.getSigner()
     const contract = new ethers.Contract(ROBOTMURALIST_ADDRESS, RobotMuralist.abi, signer)
-    await contract.safeMint(addressTo, tokenUri)
+    await contract.safeMint(address, tokenUri)
   }
 
   return (
@@ -34,7 +36,15 @@ function App() {
         { accounts }
 
         <section>
-          <button onClick={async () => await mintNft("0x069fdf69bfc74a59b1e452823a27d5fed7a0020a", "https://ipfs.io/ipfs/QmaXVg572hsAMDmzCg6KrFgnuSaCcrSmqYByfc4VtqvsGU")}>Mint NFT</button>
+          <div>
+            <label htmlFor="address">Adddress to:</label>
+            <input type="text" name="address" id="address" onChange={(e) => setAddress(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor="tokenUri">Token URI:</label>
+            <input type="text" name="tokenUri" id="tokenUri" onChange={(e) => setTokenUri(e.target.value)}/>
+          </div>
+          <button onClick={async () => await mintNft()}>Mint NFT</button>
         </section>
       </header>
 
